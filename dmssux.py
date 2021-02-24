2ca917f45f760374055bdf133f23ce025b3b1d5f
oddlyspaced <oddlyspaced1@gmail.com>
2021-02-24 14:20:02 +0530

    dmssux: Add method to crop image to remove border

--- dmssux.py
+++ dmssux.py
@@ -6,4 +6,12 @@ def conver_to_grayscale(file):
     img_buffer = Image.open(file).convert('LA')
     img_buffer.save('greyscale.png')
 
+# source image is 100x30
+# we trim out 3 px worth border from all sides
+def remove_border(file):
+    img_buffer = Image.open(file)
+    cropped = img_buffer.crop((3, 3, 97, 27))
+    cropped.save('greyscale_cropped.png')
+
 conver_to_grayscale(image_src)
+remove_border('greyscale.png')
