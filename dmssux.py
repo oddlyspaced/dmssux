f7122d64de738d3f698df31c8c0219e4f679c322
oddlyspaced <oddlyspaced1@gmail.com>
2021-02-24 14:35:55 +0530

    dmssux: Add method to improve colors of image

--- dmssux.py
+++ dmssux.py
@@ -1,6 +1,7 @@
 from PIL import Image
 
 image_src = '/home/hardik/Projects/Python-Stuff/dmssux/original/12.png'
+white_threshold = 200
 
 def conver_to_grayscale(file):
     img_buffer = Image.open(file).convert('LA')
@@ -13,5 +14,18 @@ def remove_border(file):
     cropped = img_buffer.crop((3, 3, 97, 27))
     cropped.save('greyscale_cropped.png')
 
+def improve_color(file):
+    img_buffer = Image.open(file)
+    width, height = img_buffer.size
+    for x in range(0, width):
+        for y in range(0, height):
+            darkness = img_buffer.getpixel((x, y))[0]
+            if (darkness < white_threshold) :
+                img_buffer.putpixel((x, y), (0, 255))
+            else :
+                img_buffer.putpixel((x, y), (255, 255))
+    img_buffer.save('grayscale_enhanced.png')
+
 conver_to_grayscale(image_src)
 remove_border('greyscale.png')
+improve_color('greyscale_cropped.png')
